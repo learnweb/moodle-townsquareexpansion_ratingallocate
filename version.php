667f4cf12fe6cc52efbd985775a28f48ae0a5d0c
tamaro <tamarowalter@yahoo.de>
2025-06-24 23:22:31 +0200

    new workflow

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined ('MOODLE_INTERNAL') || die();
 $plugin->component = 'townsquareexpansion_ratingallocate';
 $plugin->dependencies = ['local_townsquaresupport' => ANY_VERSION, 'mod_ratingallocate' => ANY_VERSION];
 $plugin->release = '0.1.0';
-$plugin->version = 2024050900;
+$plugin->version = 2024062400;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_ALPHA;
